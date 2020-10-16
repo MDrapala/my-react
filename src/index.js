0d625c5fdc383dceefd23224c569e1416db037c6
MDrapala <drapalamathieu95@gmail.com>
2020-10-16 09:32:20 +0200

    feat(home): add new files

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import Home from './components/home';
 
 import './index.scss';
 
+const datas = [{
+  id: '12345',
+  name: 'Cyril',
+  phone: '0606060606',
+}, {
+  id: '1234',
+  name: 'Matt',
+  phone: '0707070707',
+}, {
+  id: '123',
+  name: 'David',
+  phone: '0808080808',
+}];
+
 const App = () => (
   <div>
     <ul>
